1334c0681bcfb64bf1b97743c97de20ddc8c9bde
tony <atony.mtz@gmail.com>
2019-08-26 18:47:20 -0700

    logging

--- server/logger.js
+++ server/logger.js
@@ -1,27 +1,18 @@
 const Sentry = require('@sentry/node');
 Sentry.init({ dsn: 'https://b1f48cd0c85a4bf6b951bcb0f780cc55@sentry.io/1542244' });
 
-var winston = require('winston');
-
-//
-// Requiring `winston-papertrail` will expose
-// `winston.transports.Papertrail`
-//
+const winston = require('winston');
 require('winston-papertrail').Papertrail;
 
-var winstonPapertrail = new winston.transports.Papertrail({
-  host: 'logs4.papertrailapp.com',
-  port: 48158
+var logger = new winston.transports.Papertrail({
+  host: 'logs4.papertrailapp.com', // you get this from papertrail account
+  port: 48158, //you get this from papertrail account
+  colorize: true,
+  handleExceptions: true
 });
 
-winstonPapertrail.on('error', function(err) {
-  // Handle, report, or silently ignore connection errors and failures
-});
+const obj = {
+  info: message => logger.log('info', message)
+};
 
-var logger = new winston.Logger({
-  transports: [winstonPapertrail]
-});
-
-logger.info('this is my message');
-
-module.exports = logger;
+module.exports = obj;
