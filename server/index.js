0428624f0975544b69d7eb34af62fade80bf1fb7
tony <atony.mtz@gmail.com>
2019-08-26 20:02:38 -0700

    logging

--- server/index.js
+++ server/index.js
@@ -1,21 +1,24 @@
 const logger = require('./logger');
 const fetcher = require('./fetcher');
 
+logger.info("YOLO");
 logger.info(process.env);
+logger.info(process.env.PORT);
+logger.info("YOLO 2");
 
-const sysRun = new Promise(function (resolve) {
-  logger.info('=== Starting DOLLAR-BOT suite ===');
-  logger.info('> Starting initial fetch');
-
-  fetcher.fetchAll().then(() => {
-    logger.info('> Initial fetch done');
-    resolve();
-  });
-});
-
-sysRun.then(() => {
-  logger.info('> Starting services');
-  require('./web');
-  require('./discord');
-  require('./scheduler');
-});
+// const sysRun = new Promise(function (resolve) {
+//   logger.info('=== Starting DOLLAR-BOT suite ===');
+//   logger.info('> Starting initial fetch');
+//
+//   fetcher.fetchAll().then(() => {
+//     logger.info('> Initial fetch done');
+//     resolve();
+//   });
+// });
+//
+// sysRun.then(() => {
+//   logger.info('> Starting services');
+//   require('./web');
+//   require('./discord');
+//   require('./scheduler');
+// });
