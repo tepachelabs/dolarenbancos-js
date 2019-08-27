0bd82275afc211a91be55725b612e8c86b0a2d63
tony <atony.mtz@gmail.com>
2019-08-26 19:24:23 -0700

    logging

--- server/logger.js
+++ server/logger.js
@@ -1,20 +1,27 @@
 const Sentry = require('@sentry/node');
 Sentry.init({ dsn: 'https://b1f48cd0c85a4bf6b951bcb0f780cc55@sentry.io/1542244' });
 
-// const winston = require('winston');
-// require('winston-papertrail').Papertrail;
-//
-// const winstonLogger = new winston.transports.Papertrail({
-//   host: 'logs4.papertrailapp.com',
-//   port: 48158,
-//   handleExceptions: true
-// });
-//
+var winston = require('winston');
+require('winston-papertrail').Papertrail;
+
+var winstonPapertrail = new winston.transports.Papertrail({
+  host: 'logs4.papertrailapp.com',
+  port: 48158
+});
+
+winstonPapertrail.on('error', function (err) {
+  // Handle, report, or silently ignore connection errors and failures
+});
+
+var winstonLogger = new winston.Logger({
+  transports: [winstonPapertrail]
+});
+
+winstonLogger.info('TEST ?');
+
 const logger = {
-  // info: message => winstonLogger.log('info', message),
-  // error: message => winstonLogger.log('error', message)
-  info: message => console.log(message),
-  error: message => console.log(message)
+  info: message => winstonLogger.log('info', message),
+  error: message => winstonLogger.log('error', message)
 };
 
 module.exports = logger;
