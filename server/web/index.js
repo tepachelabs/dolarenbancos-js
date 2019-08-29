70b33006a7cccefa3a74dd9f9fea9da01ba259a7
tony <atony.mtz@gmail.com>
2019-08-29 01:11:59 -0700

    better landing

--- server/web/index.js
+++ server/web/index.js
@@ -1,16 +1,30 @@
-const logger = require('../logger');
-const express = require('express');
 const path = require('path');
+const express = require('express');
+const sass = require('node-sass-middleware');
+const logger = require('../logger');
 const data = require('../data');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const isDev = process.env.NODE_ENV !== 'production';
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, './views'));
+app.use(
+  sass({
+    src: path.join(__dirname, 'scss'), //where the sass files are
+    dest: path.join(__dirname, 'public'), //where the sass files are
+    prefix: '/public',
+    outputStyle: isDev ? 'nested' : 'compressed',
+    force: isDev,
+    debug: isDev
+  })
+);
 
 app.get('/', (req, res) =>
   res.render('index', { data })
 );
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => logger.info(`* Web server [ONLINE]`));
