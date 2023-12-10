108a9f102ab934dc9abd2f51570eb4ae4a75e958
Jose Salcido <jmsalcidoaguilar@gmail.com>
2023-12-10 16:13:16 -0700

    Telegraf upgrade

--- server/telegram/index.js
+++ server/telegram/index.js
@@ -1,22 +1,23 @@
-const { Telegraf } = require('telegraf')
+const Telegraf = require('telegraf').Telegraf;
 const logger = require('../logger');
 const data = require('../data');
 
 const tokenTelegram = process.env.TELEGRAM_TOKEN;
 
 if (tokenTelegram) {
+  /** @type {Telegraf} */
   const bot = new Telegraf(tokenTelegram);
-  const response = ({ reply, message }) => {
-    const split = message.text.split(' ');
+  const response = (ctx) => {
+    const split = ctx.message.text.split(' ');
     const dollars = parseInt(split[1], 10);
 
-    reply(data.getBotMessage(isNaN(dollars) ? 1 : dollars));
+    ctx.reply(data.getBotMessage(isNaN(dollars) ? 1 : dollars));
   };
 
   bot.command('dolar', response);
   bot.command('dólar', response);
   bot.command('dollar', response);
-  bot.command('about', ({ reply }) => reply('Dolarenbancos bot. More info at https://dolarenbancos.com.'));
+  bot.command('about', (ctx) => ctx.reply('Dolarenbancos bot. More info at https://dolarenbancos.com.'));
 
   bot.catch((err) => {
     logger.error(err);
