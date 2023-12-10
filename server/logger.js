8411c85a253b6f39cfa0d73ae9cdb5f1ac49099a
Jose Salcido <jmsalcidoaguilar@gmail.com>
2023-12-10 13:22:32 -0700

    Remove papertrail now

--- server/logger.js
+++ server/logger.js
@@ -1,6 +1,5 @@
 require('./rollbar');
 const Sentry = require('@sentry/node');
-const winston = require('winston');
 
 const sentryUrl = process.env.SENTRY_URL;
 const papertrailUrl = process.env.PAPERTRAIL_URL;
@@ -14,19 +13,6 @@ if (process.env.NODE_ENV === 'production') {
   }
 
   Sentry.init({ dsn: sentryUrl });
-
-  require('winston-papertrail').Papertrail;
-
-  const winstonPapertrail = new winston.transports.Papertrail({
-    host: papertrailUrl,
-    port: papertrailPort,
-    hostname: process.env.NODE_ENV === 'production' ? 'dolarenbancos.prod' : 'dolarenbancos.dev',
-    program: 'dolarenbancos'
-  });
-
-  const winstonLogger = winston.createLogger({
-    transports: [winstonPapertrail]
-  });
 
   logger = {
     info: message => winstonLogger.log('info', message),
