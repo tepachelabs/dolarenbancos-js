bfe0e2a55b789b19b614142c627cb0c5e1b7da87
tonymtz <atony.mtz@gmail.com>
2023-12-12 11:38:34 -0700

    adds chartjs for weekly chart

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
 import { BANK, Prices } from '~/lib/constants'
 
+// TODO: make this empty object dynamically generated from the BANKS array
 export function getEmptyPricesObject (): Prices {
   return {
     banamex: { buy: 0, sell: 0 },
@@ -19,6 +20,7 @@ export function formatPrice (price: number): string {
   return price.toFixed(2)
 }
 
+// TODO: next two functions should be data from the database instead
 export function translateBankIdToDisplay (bankId: BANK): string {
   switch (bankId) {
     case 'inbursa':
@@ -38,18 +40,40 @@ export function translateBankIdToDisplay (bankId: BANK): string {
   }
 }
 
+export function translateBankIdToBackgroundColor (bankId: BANK): string {
+  switch (bankId) {
+    case 'inbursa':
+      return 'rgb(9,117,243)'
+    case 'banamex':
+      return 'rgb(168,38,38)'
+    case 'bbva':
+      return 'rgb(57,219,248)'
+    case 'banxico':
+      return 'rgb(65,67,72)'
+    case 'intercam':
+      return 'rgb(50,171,44)'
+    case 'transferwise':
+      return 'rgb(243,99,9)'
+    default:
+      return 'rgb(243,212,9)'
+  }
+}
+
 export function formatServerDate (date: Date): string {
-  return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
+  return `${ date.getMonth() + 1 }/${ date.getDate() }/${ date.getFullYear() }`
 }
 
 interface ChartFormat {
-  name: BANK,
-  data: number[]
+  label: string,
+  backgroundColor: string,
+  borderColor: string,
+  data: Array<number | null>,
+  tension: number,
 }
 
 export function convertToChartFormat (week: Record<string, Prices>): ChartFormat[] {
   const chartFormat: ChartFormat[] = []
-  const bankWeeklyPrices = new Map<BANK, number[]>()
+  const bankWeeklyPrices = new Map<BANK, ChartFormat['data']>()
 
   for (const date in week) {
     const prices = week[date]
@@ -62,16 +86,45 @@ export function convertToChartFormat (week: Record<string, Prices>): ChartFormat
         bankWeeklyPrices.set(bank, [])
       }
 
-      bankWeeklyPrices.get(bank)?.unshift(price.buy)
+      // returns null to avoid showing 0 values on the chart
+      bankWeeklyPrices.get(bank)?.unshift(price.buy || null)
     }
   }
 
+  // creates the options for the chart
   bankWeeklyPrices.forEach(
     (price, bank) => chartFormat.push({
-      name: bank,
+      // label to render in the tooltip
+      label: translateBankIdToDisplay(bank),
+      // dataset
       data: price,
-    })
+      // dot's color
+      backgroundColor: translateBankIdToBackgroundColor(bank),
+      // line's color
+      borderColor: translateBankIdToBackgroundColor(bank),
+      // how smooth the line is
+      tension: 0.5,
+    }),
   )
 
   return chartFormat
+}
+
+const month = [
+  'Ene',
+  'Feb',
+  'Mar',
+  'Abr',
+  'May',
+  'Jun',
+  'Jul',
+  'Ago',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dic',
+]
+
+export function prettifyDate (date: Date): string {
+  return `${ date.getDate() } ${ month[date.getMonth()] }`
 }
