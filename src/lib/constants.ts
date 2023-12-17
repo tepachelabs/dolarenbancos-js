dfc616d6f451e024f6ebaeaf87b6ecebce78219a
tonymtz <atony.mtz@gmail.com>
2023-12-17 16:59:27 -0700

    meta tags

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -19,3 +19,12 @@ export type Prices = Record<BANK, {
   buy: number;
   sell: number;
 }>
+
+export const meta = {
+  title: 'El Dólar en Bancos de México',
+  // eslint-disable-next-line max-len
+  description: 'El precio del dólar en diferentes bancos de México. Calculadora de dólares a pesos mexicanos. Bot de Telegram y Discord para consultar el precio del dólar al instante.',
+  keywords: 'dolar, precio, pesos, mexicanos, bancos, mxn, precios',
+  abstract: 'El precio del dólar en los distintos bancos de México',
+  url: 'https://dolarenbancos.com',
+}
